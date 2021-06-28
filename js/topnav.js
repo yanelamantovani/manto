945661aa15a4f4fbd558ebe3efd53bdaa0edd1ed
Palilu <palilu@icloud.com>
2021-06-28 16:09:58 -0300

    Partial Render

--- js/topnav.js
+++ js/topnav.js
@@ -1,5 +1,28 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function(event) {
+    
+    let page = "portada";
+    
+    loadContent();
+    
+    document.querySelector("#portada").addEventListener("click", function() {
+        loadContent("portada");
+    });
+    
+    async function loadContent(event) {
+        //event.preventDefault();
+        try {
+            let response = await fetch(`${page}.html`)
+            if (response.ok) {
+                let content = await response.text();
+            } else {
+                console.log("Status de respuesta inválido");
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
     document.querySelector("#topNav").addEventListener("click", function() {
         document.querySelector("nav").classList.toggle("visible");
     });
